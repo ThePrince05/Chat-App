5d6e49cc2d6d74dd505e7a279ff2a88b9b79addb
ThePrince05 <110377389+ThePrince05@users.noreply.github.com>
2024-11-29 05:42:11 +0200

    Set up project structure.

---
+++ Chat App/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Chat_App.MVVM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App.MVVM.ViewModel
+{
+     class MainViewModel
+    {
+        public RelayCommand MyProperty { get; set; }
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
