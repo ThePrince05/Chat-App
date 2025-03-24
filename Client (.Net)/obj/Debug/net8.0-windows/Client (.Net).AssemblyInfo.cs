2d51e4da273a9c1d77174116f6f47c8523d285ef
Michael <Michaelmosako17@gmail.com>
2025-03-24 02:28:34 +0200

    .

--- Client (.Net)/obj/Debug/net8.0-windows/Client (.Net).AssemblyInfo.cs
+++ Client (.Net)/obj/Debug/net8.0-windows/Client (.Net).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client (.Net)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457c83e29233506d7362bc717e4b0a15a476cf08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adfbc94a28f53350d9aba6976022adf3b81e85c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client (.Net)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client (.Net)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
